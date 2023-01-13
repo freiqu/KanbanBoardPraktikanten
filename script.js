40bb072835f37ea3574855c5d732a0db111d1d4e
Andreas <straushans0@gmail.com>
2023-01-13 11:30:42 +0100

    mehr Details und eingabetaste

--- script.js
+++ script.js
@@ -9,36 +9,79 @@ function addItem(e) {
     let list = document.getElementById("todo");
     let input = document.getElementById("input");
     let inputValue = input.value;
+    let person = input_person.value;
+    let datum = input_datum.value;
     if (inputValue != "") {
         let listItem = document.createElement("div"); // <div></div>
         listItem.classList += "list-item"; // <div class="list-item"></div>
-        listItem.innerHTML = inputValue; // <div class="list-item">test</div>
+        listItem.innerHTML = "<b>Aufgabe:</b>" + inputValue + "<br>" + "<b>Person:</b>" + person + "<br>" + "<b>Datum:</b>" + datum; // <div class="list-item">test</div>
         let buttonItem = document.createElement("button");
         buttonItem.classList += 'button';
-        buttonItem.innerHTML = "weiter";
-        buttonItem.addEventListener("click", (ev) => {
-            verschiebenNachInprogress(ev, inputValue, listItem, buttonItem)
-        })
+        buttonItem.innerHTML = "In Progress";
+        let buttonItem_2 = document.createElement("button");
+        buttonItem_2.classList += 'button';
+        buttonItem_2.innerHTML = "Delete";
+        // buttonItem.addEventListener("click", (ev) => {
+        //     verschiebenNachInprogress(ev, listItem, buttonItem)
+        // })
         listItem.append(buttonItem);
+        listItem.append(buttonItem_2);
         input.value = "";
+        input_person.value = "";
+        input_datum.value = "";
         list.appendChild(listItem);
+        buttonItem_2.addEventListener("click", (ev) => {
+            list.removeChild(listItem);
+        })
+        buttonItem.addEventListener("click", (ev) => {
+            verschiebenNachInprogress(ev, listItem, buttonItem, buttonItem_2)
+        })
     }
 }
 
-function verschiebenNachInprogress(ev, text, listItem, buttonItem) {
-    console.log(text)
+function verschiebenNachInprogress(ev, listItem, buttonItem, buttonItem_2) {
     let list = document.getElementById("inprogress");
     list.appendChild(listItem);
+    buttonItem.innerHTML = "Done";
+    buttonItem_2.addEventListener("click", (ev) => {
+        list.removeChild(listItem);
+    })
     buttonItem.addEventListener("click", (evn) => {
-        verschiebenNachDone(ev, text, listItem, buttonItem)
+        verschiebenNachDone(ev, listItem, buttonItem, buttonItem_2)
     })
 }
 
-function verschiebenNachDone(evn, text, listItem, buttonItem) {
+function verschiebenNachDone(evn, listItem, buttonItem, buttonItem_2) {
     let list = document.getElementById("done");
+    listItem.remove(buttonItem_2)
     list.appendChild(listItem);
+    buttonItem.innerHTML = "Delete";
     buttonItem.addEventListener("click", (e) => {
         list.removeChild(listItem);
     })
 }
 
+function myFunction(){
+input.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      addItem();
+    }
+})}
+
+function myFunction_2(){
+input_person.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      addItem();
+    }
+})}
+
+function myFunction_3(){
+input_datum.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+          addItem();
+    }
+})}
+
+localStorage.setItem("inhalt", body);
+
+document.getElementById("body").innerHTML = localStorage.getItem("inhalt");
